a38562728c6b38d5591c3625cc5669eda1238807
Chris Wubbels <chris@junipercollaborative.com>
2022-03-08 11:15:17 -0600

    Adding the script.

---
+++ randommac.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+# Version 0.1 - Tested on macOS 12.2.1 - 2022-03-07
+# Generates a random MAC address, disconnects from any Wi-Fi networks, and sets the Wi-Fi
+# interface to use the random address. 
+
+# Notes: 
+# Does nothing to examine whether the MAC address is unicast, multicast, etc. YMMV
+# Does not reconnect to the Wi-Fi network. You need to do that manually in the usual way. 
+
+MACADDRESS=$(openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/./0/2; s/.$//')
+sudo /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport "en0" -z
+sudo ifconfig en0 ether $MACADDRESS
